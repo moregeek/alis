79fc44b8375cae51e455f5f073c98d9f182b23fd
Stefan Morgenthaler <stefan+dev@morgenthaler.at>
2020-06-12 09:57:55 +0200

    btrfs changed volume layout

--- alis.sh
+++ alis.sh
@@ -505,19 +505,23 @@ function partition() {
     # mount
     if [ "$FILE_SYSTEM_TYPE" == "btrfs" ]; then
         mount -o "$PARTITION_OPTIONS" "$DEVICE_ROOT" /mnt
-        btrfs subvolume create /mnt/root
-        btrfs subvolume create /mnt/home
-        btrfs subvolume create /mnt/var
-        btrfs subvolume create /mnt/snapshots
+        btrfs subvolume create /mnt/@
+        btrfs subvolume create /mnt/@snapshots
+        btrfs subvolume create /mnt/@var
+        btrfs subvolume create /mnt/@home
+        btrfs subvolume create /mnt/@home_snapshots
+
         umount /mnt
+        mount -o "subvol=@,$PARTITION_OPTIONS,compress=lzo" "$DEVICE_ROOT" /mnt
 
-        mount -o "subvol=root,$PARTITION_OPTIONS,compress=lzo" "$DEVICE_ROOT" /mnt
-
-        mkdir /mnt/{boot,home,var,snapshots}
+        mkdir /mnt/{boot,home,var,.snapshots}
         mount -o "$PARTITION_OPTIONS" "$PARTITION_BOOT" /mnt/boot
-        mount -o "subvol=home,$PARTITION_OPTIONS,compress=lzo" "$DEVICE_ROOT" /mnt/home
-        mount -o "subvol=var,$PARTITION_OPTIONS,compress=lzo" "$DEVICE_ROOT" /mnt/var
-        mount -o "subvol=snapshots,$PARTITION_OPTIONS,compress=lzo" "$DEVICE_ROOT" /mnt/snapshots
+        mount -o "subvol=@snapshots,$PARTITION_OPTIONS,compress=lzo" "$DEVICE_ROOT" /mnt/.snapshots
+        mount -o "subvol=@var,$PARTITION_OPTIONS,compress=lzo" "$DEVICE_ROOT" /mnt/var
+        mount -o "subvol=@home,$PARTITION_OPTIONS,compress=lzo" "$DEVICE_ROOT" /mnt/home
+
+        mkdir /mnt/home/.snapshots
+        mount -o "subvol=@home_snapshots,$PARTITION_OPTIONS,compress=lzo" "$DEVICE_ROOT" /mnt/home/.snapshots
     else
         mount -o "$PARTITION_OPTIONS" "$DEVICE_ROOT" /mnt
 
@@ -857,7 +861,7 @@ function bootloader() {
         CMDLINE_LINUX="cryptdevice=PARTUUID=$PARTUUID_ROOT:$LUKS_DEVICE_NAME$BOOTLOADER_ALLOW_DISCARDS"
     fi
     if [ "$FILE_SYSTEM_TYPE" == "btrfs" ]; then
-        CMDLINE_LINUX="$CMDLINE_LINUX rootflags=subvol=root"
+        CMDLINE_LINUX="$CMDLINE_LINUX rootflags=subvol=@"
     fi
     if [ "$KMS" == "true" ]; then
         case "$DISPLAY_DRIVER" in
